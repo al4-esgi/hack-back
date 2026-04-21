909464b605bca4b7db6eae4520eb49df3ba5a162
Hoareau.Steevy <shoareau@dev-id.fr>
2026-04-21 10:19:53 +0200

    feat: add compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { EnvironmentVariables, validateEnv } from './_utils/config/env.config';
+import { ConfigModule } from '@nestjs/config';
+import { validateEnv } from './_utils/config/env.config';
 import { NodemailerModule } from './nodemailer/nodemailer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ validate: validateEnv, isGlobal: true }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService<EnvironmentVariables, true>) => ({
-        uri: configService.get('MONGODB_URL'),
-      }),
-    }),
     AuthModule,
     UsersModule,
     NodemailerModule,
